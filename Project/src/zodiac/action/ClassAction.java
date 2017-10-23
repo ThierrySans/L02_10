09d1c6d0ed3eef22888ba308b42fbec45d9cfc77
Matthew Lau <matthewyathin.lau@mail.utoronto.ca>
2017-10-22 21:05:08 -0400

    Fixed issues with the actions classes not creating the dao objects before trying to use them

--- Project/src/zodiac/action/ClassAction.java
+++ Project/src/zodiac/action/ClassAction.java
@@ -4,13 +4,11 @@ import zodiac.dao.ClassDao;
 
 public class ClassAction {
 
-  private ClassDao classDao;
-
   /**
    * Add API to create a Class object.
    */
   public String addClass(String courseCode, String className) {
-    return classDao.addEditClass(courseCode, className);
+    return new ClassDao().addEditClass(courseCode, className);
   }
 
 
--- Project/src/zodiac/action/StudentAction.java
+++ Project/src/zodiac/action/StudentAction.java
@@ -5,13 +5,11 @@ import zodiac.definition.Student;
 
 public class StudentAction {
 
-  private StudentDao studentDao;
-
   /**
    * API to add Student objects to a Class object.
    */
   public String addStudentToClass(Student student, String courseCode) {
-    return studentDao.addStudent(student, courseCode);
+    return new StudentDao().addStudent(student, courseCode);
 
   }
 
