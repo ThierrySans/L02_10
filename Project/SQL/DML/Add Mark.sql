9d12185ab34d7e41d98aca220226bc1f8463e4e9
Matthew Lau <matthewyathin.lau@mail.utoronto.ca>
2017-11-08 21:22:42 -0500

    Updated Add_Mark Function to automatically increase used attempt

--- Project/SQL/DML/Add Mark.sql
+++ Project/SQL/DML/Add Mark.sql
@@ -2,6 +2,10 @@
  	RETURNS varchar AS $$
  	BEGIN
  		INSERT INTO UserAssignMarkMap (UTOR_Id, Assignment_Id,Mark) VALUES (Users, assignmentID,mark);
+ 		UPDATE userassignmarkmap
+ 		  SET UsedAttempts = (
+ 		    (SELECT coalesce(MAX(usedattempts), 0) FROM userassignmarkmap WHERE utor_id = Users AND assignment_id = assignmentID) + 1
+ 		   ) WHERE utor_id = UTOR_Id AND assignment_id = assignmentID;
  		RETURN 'Answer Added';
  	END;
  	$$ LANGUAGE plpgsql;
