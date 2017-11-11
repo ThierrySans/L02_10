824f7b6c16080add25a093794d3a5436a4273fd1
Matthew Lau <matthewyathin.lau@mail.utoronto.ca>
2017-11-11 18:35:08 -0500

    Formatted style for code relating to edit assignment GUI

--- Project/src/zodiac/gui/GetAssignmentsMenu.java
+++ Project/src/zodiac/gui/GetAssignmentsMenu.java
@@ -1,94 +1,102 @@
 package zodiac.gui;
 
-import zodiac.dao.coursework.AssignmentDao;
-import zodiac.definition.coursework.Assignment;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import zodiac.dao.coursework.AssignmentDao;
+import zodiac.definition.coursework.Assignment;
 
 //import com.intellij.ui.components.JBScrollPane;
 //import com.intellij.ui.components.JBList;
 
 
 public class GetAssignmentsMenu {
-    private JPanel panel;
-    private JList assignList;
-    private DefaultListModel model;
 
-    /**
-     * Generate the contents of the Assignment manager menu.
-     */
-    public JPanel generateContents() {
-        this.panel = new JPanel();
-        this.panel.setLayout(new BoxLayout(this.panel, BoxLayout.Y_AXIS));
+  private JPanel panel;
+  private JList assignList;
+  private DefaultListModel model;
 
-        JPanel searchPanel = new JPanel(new FlowLayout());
-        JLabel prompt = new JLabel("Enter a Course Code: ");
-        JTextField textCourseCode = new JTextField();
-        textCourseCode.setPreferredSize(new Dimension(300, 25));
-        textCourseCode.setMinimumSize(new Dimension(300, 25));
-        JButton submit = new JButton("Search");
+  /**
+   * Generate the contents of the Assignment manager menu.
+   */
+  public JPanel generateContents() {
+    this.panel = new JPanel();
+    this.panel.setLayout(new BoxLayout(this.panel, BoxLayout.Y_AXIS));
 
-        submit.addActionListener(new getAssignmentsListener(textCourseCode));
+    JPanel searchPanel = new JPanel(new FlowLayout());
+    JLabel prompt = new JLabel("Enter a Course Code: ");
+    JTextField textCourseCode = new JTextField();
+    textCourseCode.setPreferredSize(new Dimension(300, 25));
+    textCourseCode.setMinimumSize(new Dimension(300, 25));
+    JButton submit = new JButton("Search");
 
-        searchPanel.add(prompt);
-        searchPanel.add(textCourseCode);
-        searchPanel.add(submit);
+    submit.addActionListener(new getAssignmentsListener(textCourseCode));
 
-        this.model = new DefaultListModel();
+    searchPanel.add(prompt);
+    searchPanel.add(textCourseCode);
+    searchPanel.add(submit);
 
-        assignList = new JList(model);
-        JScrollPane scrollPane = new JScrollPane(assignList);
+    this.model = new DefaultListModel();
 
+    assignList = new JList(model);
+    JScrollPane scrollPane = new JScrollPane(assignList);
 
-        this.panel.add(searchPanel);
-        this.panel.add(scrollPane);
+    this.panel.add(searchPanel);
+    this.panel.add(scrollPane);
 
-        addMouseListener(assignList);
+    addMouseListener(assignList);
 
-        return this.panel;
-    }
+    return this.panel;
+  }
 
-    // Open assignment editor when on double click assignment
-    private void addMouseListener(JList list) {
-        list.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent evt) {
-                JList list = (JList) evt.getSource();
-                if (evt.getClickCount() == 2) {
-                    // Double-click detected
-                    int index = list.locationToIndex(evt.getPoint());
-                    Assignment assignment = (Assignment)model.get(index);
-                    new EditAssignmentMenu(assignment).setVisible(true);
-                }
-            }
-        });
-    }
-
-    private class getAssignmentsListener implements ActionListener {
-        private JTextField courseCode;
-
-        getAssignmentsListener(JTextField code) {
-            this.courseCode = code;
+  // Open assignment editor when on double click assignment
+  private void addMouseListener(JList list) {
+    list.addMouseListener(new MouseAdapter() {
+      public void mouseClicked(MouseEvent evt) {
+        JList list = (JList) evt.getSource();
+        if (evt.getClickCount() == 2) {
+          // Double-click detected
+          int index = list.locationToIndex(evt.getPoint());
+          Assignment assignment = (Assignment) model.get(index);
+          new EditAssignmentMenu(assignment).setVisible(true);
         }
+      }
+    });
+  }
 
-        @Override
-        public void actionPerformed(ActionEvent actionEvent) {
-            System.out.println(this.courseCode);
-            List<Assignment> results = new AssignmentDao().getAssignments(this.courseCode.getText());
-            model.clear();
-            for (Assignment a : results) {
+  private class getAssignmentsListener implements ActionListener {
+
+    private JTextField courseCode;
+
+    getAssignmentsListener(JTextField code) {
+      this.courseCode = code;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+      System.out.println(this.courseCode);
+      List<Assignment> results = new AssignmentDao().getAssignments(this.courseCode.getText());
+      model.clear();
+      for (Assignment a : results) {
 //                System.out.println(a.getName());
-                // What the model displays is defined in Assignment.ToString
-                model.addElement(a);
-            }
+        // What the model displays is defined in Assignment.ToString
+        model.addElement(a);
+      }
 
-            assignList = new JList(model);
-            panel.revalidate();
-        }
+      assignList = new JList(model);
+      panel.revalidate();
     }
+  }
 }
