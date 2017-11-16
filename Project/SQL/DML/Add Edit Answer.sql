1bdf76b99723cfeab67d1a3ab152d4f0d51216b6
stone <stone.jin@mail.utoronto.ca>
2017-11-16 10:42:47 -0500

    Add storing temporal answer sql

---
+++ Project/SQL/DML/Add Edit Answer.sql
@@ -0,0 +1,29 @@
+ CREATE OR REPLACE FUNCTION Add_Answer(Users varchar(100), assignmentID int , questionId int, answer varchar(255))
+ 	RETURNS varchar AS $$
+ 	BEGIN
+	 	IF EXISTS (SELECT 1 FROM UserAssignQuesAnsMap WHERE UTOR_Id=Users AND Assignment_Id=assignmentID AND Question_Id=questionId) THEN
+ 			UPDATE UserAssignQuesAnsMap SET Answer = answer, Temp_Answer = NULL WHERE UTOR_Id=Users AND Assignment_Id=assignmentID AND Question_Id=questionId;
+ 			RETURN 'Offical Answer Modified';
+ 		ELSE
+		  	INSERT INTO UserAssignQuesAnsMap (UTOR_Id, Assignment_Id,Question_Id,Answer,Temp_Answer) VALUES (Users, assignmentID,questionId,answer,NULL);
+ 			RETURN 'Offical Answer Added';
+ 		END IF;
+
+
+ 	END;
+ 	$$ LANGUAGE plpgsql;
+
+ CREATE OR REPLACE FUNCTION Add_Temp_Answer(Users varchar(100), assignmentID int , questionId int,temp_answer varchar(255))
+ 	RETURNS varchar AS $$
+ 	BEGIN
+	 	IF EXISTS (SELECT 1 FROM UserAssignQuesAnsMap WHERE UTOR_Id=Users AND Assignment_Id=assignmentID AND Question_Id=questionId) THEN
+ 			UPDATE UserAssignQuesAnsMap SET Temp_Answer=temp_answer WHERE UTOR_Id=Users AND Assignment_Id=assignmentID AND Question_Id=questionId;
+ 			RETURN 'Temporal Answer Modified';
+ 		ELSE
+		  	INSERT INTO UserAssignQuesAnsMap (UTOR_Id, Assignment_Id,Question_Id,Temp_Answer) VALUES (Users, assignmentID,questionId,temp_answer);
+ 			RETURN 'Temporal Answer Added';
+ 		END IF;
+
+
+ 	END;
+ 	$$ LANGUAGE plpgsql;
