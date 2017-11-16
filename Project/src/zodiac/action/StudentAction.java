4d69a7202aafa2e91bce068dc299935d4ce7c7f0
Matthew Lau <matthewyathin.lau@mail.utoronto.ca>
2017-11-15 20:47:22 -0500

    Added permission check to student action and fixed unable to save mark error

--- Project/src/zodiac/action/StudentAction.java
+++ Project/src/zodiac/action/StudentAction.java
@@ -3,9 +3,11 @@ package zodiac.action;
 import java.util.TreeMap;
 import zodiac.dao.StudentDao;
 import zodiac.dao.coursework.AssignmentDao;
+import zodiac.definition.MessageConstants;
 import zodiac.definition.Student;
 import zodiac.definition.coursework.Assignment;
 import zodiac.definition.coursework.Question;
+import zodiac.util.ActiveUser;
 
 public class StudentAction {
 
@@ -13,25 +15,33 @@ public class StudentAction {
    * API to get a Student obejct
    */
   public static Student getStudent(String userID, String courseId) {
-    return StudentDao.getStudent(userID, courseId);
+    if (ActiveUser.INSTANCE.canRead(courseId)) {
+      return StudentDao.getStudent(userID, courseId);
+    } else {
+      return null;
+    }
   }
 
   /**
    * API to add answer for specific assignment and answer
    *
    * @param student the answer is added under this student
-   * @param assignmentId the assignment id this answer belong
-   * @param QA the question to answers map
+   * @param assignment the assignment this answer belong
+   * @param qa the question to answers map
    * @return true if added succ, false otherwise
    */
   public static boolean addAnswerToAssignment(Student student, Assignment assignment,
-      TreeMap<Question, String> QA) {
-
-    for (Question question : QA.keySet()) {
-      StudentDao
-          .addAnswerToQuestion(student, assignment.getId(), question.getQid(), QA.get(question));
+      TreeMap<Question, String> qa) {
+    if (ActiveUser.INSTANCE.canRead(
+        new AssignmentDao().getCourseOfAssignment(assignment.getId()))) {
+      for (Question question : qa.keySet()) {
+        StudentDao
+            .addAnswerToQuestion(student, assignment.getId(), question.getQid(), qa.get(question));
+      }
+      return true;
+    } else {
+      return false;
     }
-    return true;
   }
 
   /**
@@ -56,7 +66,8 @@ public class StudentAction {
    * API for saving mark into database
    */
   public static void saveMark(Student student, Assignment ass, Integer mark) {
-    if (usedMaxAttempts(student, ass)) {
+    if (!usedMaxAttempts(student, ass)
+        && ActiveUser.INSTANCE.canRead(new AssignmentDao().getCourseOfAssignment(ass.getId()))) {
       // If students finds a way to complete an assignment even after using all attempts,
       // do not save mark
       StudentDao.saveMark(student, ass, mark);
@@ -83,7 +94,11 @@ public class StudentAction {
    * API to add Student objects to a Class object.
    */
   public String addStudentToClass(Student student, String courseCode) {
-    return new StudentDao().addStudent(student, courseCode);
+    if (ActiveUser.INSTANCE.canWrite(courseCode)) {
+      return new StudentDao().addStudent(student, courseCode);
+    } else {
+      return MessageConstants.NO_PERMISSION_MESSAGE;
+    }
 
   }
 }
