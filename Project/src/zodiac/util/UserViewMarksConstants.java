f7197726bb38ce8c758306f934bfe6611c760fb3
David Wan <dave.wan@mail.utoronto.ca>
2017-11-18 21:13:27 -0500

    Users can now view their marks in a table

--- Project/src/zodiac/util/UserViewMarksConstants.java
+++ Project/src/zodiac/util/UserViewMarksConstants.java
@@ -7,11 +7,11 @@ public final class UserViewMarksConstants {
      */
     public static final String USER_VIEW_MARKS_ASS_ID = "Assignment ID";
     public static final String USER_VIEW_MARKS_ASS_NAME = "Assignment Name";
-    public static final String USER_VIEW_MARKS_ASS_COURSE = "Course";
     public static final String USER_VIEW_MARKS_ASS_MARK = "Mark";
     public static final String USER_VIEW_MARKS_COLUMNS[] = {USER_VIEW_MARKS_ASS_ID,
             USER_VIEW_MARKS_ASS_NAME,
-            USER_VIEW_MARKS_ASS_COURSE,
             USER_VIEW_MARKS_ASS_MARK};
     public static final String USER_VIEW_MARKS_ASS_SEARCH_TEXT = "Enter a Course Code: ";
+    public static final String USER_VIEW_MARKS_SEARCH = "Search";
+    public static final String USER_VIEW_MARKS_BACK = "Back to Main Menu";
 }
