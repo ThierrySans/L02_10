da995ac6f65ac70269267fde136a6d1a03bc845d
Matthew Lau <matthewyathin.lau@mail.utoronto.ca>
2017-11-23 22:57:43 -0500

    Changed Questions structure, table and DAO with type and automark

--- Project/src/zodiac/definition/coursework/Question.java
+++ Project/src/zodiac/definition/coursework/Question.java
@@ -7,12 +7,17 @@ import java.util.List;
  * Created by radiantwings on 10/24/17. This class represents a Question which would be included in
  * >= 0 Assignments
  */
-public class Question implements Comparable{
+public class Question implements Comparable {
+
+  public static final String MULTIPLE_CHOICE = "Multiple Choice";
+  public static final String TEXT_FIELD = "Text Field";
 
   private int qid;
   private List<String> answerList;
   private String question;
   private String correctAnswer;
+  private String questionType;
+  private boolean autoMark;
 
   // TODO: Add Javadoc
   public Question(int qid) {
@@ -50,10 +55,26 @@ public class Question implements Comparable{
     this.question = question;
   }
 
+  public String getQuestionType() {
+    return questionType;
+  }
+
+  public void setQuestionType(String questionType) {
+    this.questionType = questionType;
+  }
+
+  public boolean isAutoMark() {
+    return autoMark;
+  }
+
+  public void setAutoMark(boolean autoMark) {
+    this.autoMark = autoMark;
+  }
+
   @Override
   public int compareTo(Object o) {
-      // TODO Auto-generated method stub
-      return this.qid - ((Question)o).qid;
+    // TODO Auto-generated method stub
+    return this.qid - ((Question) o).qid;
   }
 
   @Override
