5cf86732b85ac84e5e3357994215a65fa239cbd1
Matthew Lau <matthewyathin.lau@mail.utoronto.ca>
2017-11-26 17:23:38 -0500

    UI now works with text field questions

--- Project/src/zodiac/gui/user/AssignmentUi.java
+++ Project/src/zodiac/gui/user/AssignmentUi.java
@@ -8,7 +8,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -18,15 +20,20 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import org.apache.commons.lang3.StringUtils;
 import zodiac.action.StudentAction;
 import zodiac.definition.Student;
 import zodiac.definition.coursework.Assignment;
 import zodiac.definition.coursework.Question;
+import zodiac.definition.coursework.QuestionTypeConstants;
 import zodiac.util.PDFGenerator;
 
 public class AssignmentUi extends JFrame implements ActionListener {
 
+  private Map<Integer, String> textFieldAnswers;
+  private Map<Integer, String> textFieldUserAnswers;
   private ArrayList<Integer> answers;
   private ArrayList<JRadioButton> rdbts;
   private JButton btnSaveNext;
@@ -36,6 +43,7 @@ public class AssignmentUi extends JFrame implements ActionListener {
   private ButtonGroup group;
   private JScrollPane scrollPane;
   private JPanel contentPane;
+  private JTextField textField;
   private Assignment assignment;
   private List<Question> questions;
   private TreeMap<Question, List<String>> qas;
@@ -100,6 +108,8 @@ public class AssignmentUi extends JFrame implements ActionListener {
       qas.put(question, question.getAnswerList());
     }
     // init temporal answer
+    textFieldAnswers = new HashMap<>();
+    textFieldUserAnswers = new HashMap<>();
 
     TreeMap<Question, String> temporalAnswer = new StudentAction()
         .fetchTempAnswerFromAssignment(student, assignment);
@@ -117,11 +127,18 @@ public class AssignmentUi extends JFrame implements ActionListener {
         if (rs != null) {
           answers.add(rs);
         }
-
+        if (question.getQuestionType().equals(QuestionTypeConstants.TEXT_FIELD)) {
+          textFieldAnswers.put(question.getQid(), temporalAnswer.get(question));
+        }
       }
 
 
     }
+
+    textField = new JTextField();
+    textField.setBounds(50, 80, 350, 60);
+    textField.setAlignmentX(CENTER_ALIGNMENT);
+    contentPane.add(textField);
 
     rdbts = new ArrayList<JRadioButton>();
     // adding answer option
@@ -149,6 +166,12 @@ public class AssignmentUi extends JFrame implements ActionListener {
 
     }
 
+    if (this.questions.get(0).getQuestionType().equals(QuestionTypeConstants.TEXT_FIELD)) {
+      textField.setText(textFieldAnswers.get(this.questions.get(0).getQid()));
+    }
+
+    setQuestionTypeVisibility(this.questions.get(0).getQuestionType());
+
     // set the rest of options invisible
     for (int j = i; j < 5; j++) {
       JRadioButton rb = rdbts.get(j);
@@ -159,6 +182,10 @@ public class AssignmentUi extends JFrame implements ActionListener {
     btnSaveNext.addActionListener(this);
     if (this.questions.size() == 1) {
       btnSaveNext.setText("Submit");
+    }
+
+    if (questions.get(0).getQuestionType().equals(QuestionTypeConstants.TEXT_FIELD)) {
+      btnSaveNext.setEnabled(true);
     }
 
     btnPrev.addActionListener(this);
@@ -193,7 +220,13 @@ public class AssignmentUi extends JFrame implements ActionListener {
         TreeMap<Question, String> qa = new TreeMap<Question, String>();
         for (int i = 0; i < questions.size(); i++) {
           Question q = questions.get(i);
-          String a = String.valueOf(rdbts.get(answers.get(i)).getText());
+          String a = "";
+          if (questions.get(i).getQuestionType().equals(QuestionTypeConstants.MULTIPLE_CHOICE)) {
+            a = String.valueOf(rdbts.get(answers.get(i)).getText());
+          } else if (questions.get(i).getQuestionType().equals(QuestionTypeConstants.TEXT_FIELD)) {
+            a = StringUtils.trimToEmpty(textField.getText());
+            textFieldUserAnswers.put(i, a);
+          }
           qa.put(q, a);
 
         }
@@ -203,6 +236,7 @@ public class AssignmentUi extends JFrame implements ActionListener {
         lblNewLabel.setText("Your result: " + result);
         Font f = lblNewLabel.getFont();
         lblNewLabel.setFont(new Font(f.getName(), Font.BOLD, f.getSize() + 4));
+        textField.setVisible(false);
         for (int j = 0; j < 5; j++) {
           JRadioButton rb = rdbts.get(j);
           rb.setVisible(false);
@@ -222,7 +256,16 @@ public class AssignmentUi extends JFrame implements ActionListener {
           panel.add(lblNewLabel);
           lblNewLabel = new JLabel("    Answer: " + q.getCorrectAnswer());
           panel.add(lblNewLabel);
-          lblNewLabel = new JLabel("    Your Answer: " + rdbts.get((answers.get(k))).getText());
+
+          String answer = "";
+
+          if (questions.get(k).getQuestionType().equals(QuestionTypeConstants.TEXT_FIELD)) {
+            answer = textFieldUserAnswers.get(k);
+          } else if (questions.get(k).getQuestionType().equals(QuestionTypeConstants.MULTIPLE_CHOICE)) {
+            answer = rdbts.get((answers.get(k))).getText();
+          }
+
+          lblNewLabel = new JLabel("    Your Answer: " + answer);
           panel.add(lblNewLabel);
 
         }
@@ -240,15 +283,23 @@ public class AssignmentUi extends JFrame implements ActionListener {
         contentPane.add(scrollPane);
       } else {
         // save current answer into database
+        String tempAnswer = "";
+        if (questions.get(currentAt).getQuestionType().equals(QuestionTypeConstants.MULTIPLE_CHOICE)) {
+          tempAnswer = rdbts.get(answers.get(currentAt)).getText();
+        } else if (questions.get(currentAt).getQuestionType().equals(QuestionTypeConstants.TEXT_FIELD)) {
+          tempAnswer = StringUtils.trimToEmpty(textField.getText());
+        }
         new StudentAction().addTempAnswerToQuestion(student, assignment, questions.get(currentAt),
-            rdbts.get(answers.get(currentAt)).getText());
+            tempAnswer);
 
         currentAt += 1;
         // trying to see if we have next answer
         try {
           answers.get(currentAt);
         } catch (IndexOutOfBoundsException ex) {
-          btnSaveNext.setEnabled(false);
+          if (questions.get(currentAt).getQuestionType().equals(QuestionTypeConstants.MULTIPLE_CHOICE)) {
+            btnSaveNext.setEnabled(false);
+          }
         }
         // rendering the next question and answers
         lblNewLabel
@@ -260,11 +311,16 @@ public class AssignmentUi extends JFrame implements ActionListener {
           rb.setVisible(true);
           i += 1;
         }
+        setQuestionTypeVisibility(this.questions.get(currentAt).getQuestionType());
         for (int j = i; j < 5; j++) {
           JRadioButton rb = rdbts.get(j);
           rb.setVisible(false);
         }
         // setting anwser for rendered question if we have
+        if (this.questions.get(currentAt).getQuestionType()
+            .equals(QuestionTypeConstants.TEXT_FIELD)) {
+          textField.setText(textFieldAnswers.get(currentAt));
+        }
         try {
           int answer = answers.get(currentAt);
           rdbts.get(answer).setSelected(true);
@@ -294,11 +350,16 @@ public class AssignmentUi extends JFrame implements ActionListener {
         rb.setText(answer);
         i += 1;
       }
+      setQuestionTypeVisibility(this.questions.get(currentAt).getQuestionType());
       for (int j = i; j < 5; j++) {
         JRadioButton rb = rdbts.get(j);
         rb.setVisible(false);
       }
       // trying to see if we have prev answer, and set it
+      if (this.questions.get(currentAt).getQuestionType()
+          .equals(QuestionTypeConstants.TEXT_FIELD)) {
+        textField.setText(textFieldAnswers.get(currentAt));
+      }
       try {
         int answer = answers.get(currentAt);
         rdbts.get(answer).setSelected(true);
@@ -307,8 +368,9 @@ public class AssignmentUi extends JFrame implements ActionListener {
       }
 
       if (currentAt == 0) {
-        btnPrev.setEnabled(false);
-
+        if (questions.get(currentAt).getQuestionType().equals(QuestionTypeConstants.MULTIPLE_CHOICE)) {
+          btnPrev.setEnabled(false);
+        }
       }
     } else if (e.getActionCommand() == btnPdf.getActionCommand()) {
       String path = (new PDFGenerator(assignment)).generate();
@@ -327,6 +389,21 @@ public class AssignmentUi extends JFrame implements ActionListener {
 
     } else {
       btnSaveNext.setEnabled(true);
+    }
+  }
+
+  private void setQuestionTypeVisibility(String type) {
+    if (type.equals(QuestionTypeConstants.TEXT_FIELD)) {
+      setVisibility(true, false);
+    } else if (type.equals(QuestionTypeConstants.MULTIPLE_CHOICE)) {
+      setVisibility(false, true);
+    }
+  }
+
+  private void setVisibility(boolean textFieldVisibility, boolean radioVisibility) {
+    textField.setVisible(textFieldVisibility);
+    for (JRadioButton button : rdbts) {
+      button.setVisible(radioVisibility);
     }
   }
 
