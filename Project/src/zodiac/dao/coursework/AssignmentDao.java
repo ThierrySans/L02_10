94c24ab927ca8d7449c9953937c4a58b7f2430cb
Junzhi Chen <junzhichen@Junzhis-MacBook.local>
2017-11-26 22:47:01 -0500

    add assignmentDao

--- Project/src/zodiac/dao/coursework/AssignmentDao.java
+++ Project/src/zodiac/dao/coursework/AssignmentDao.java
@@ -15,324 +15,243 @@ import zodiac.util.PostgreSqlJdbc;
 
 public class AssignmentDao {
 
-  public List<Assignment> getAssignments(String courseCode) {
-    return getAssignments(courseCode, false);
-  }
-
-  /**
-   * Gets an assignment which has the assignment ID aId.
-   * @param aId the desired assignment ID.
-   * @return the Assignment with the id aId.
-   */
-  public List<Assignment> getAssignments(Integer aId)
-  {
-    List<Assignment> assignments = new ArrayList<>();
-
-    Connection c;
-    PreparedStatement stmt;
-
-    String sql = "SELECT Id, Assignment_Name, Visibility, Max_Attempt "
-            + "FROM Assignments "
-            + "WHERE id = ? ";
-
-    try {
-      c = new PostgreSqlJdbc().getConnection();
-      stmt = c.prepareStatement(sql);
-      stmt.setInt(1, aId);
-      ResultSet rs = stmt.executeQuery();
-
-      while (rs.next())
-      {
-        int id = rs.getInt("Id");
-        String name = rs.getString("Assignment_Name");
-        Assignment assignment = new Assignment(name, id);
-//        assignment.setVisibility(rs.getString("Visibility"));
-
-        int maxAttempts = rs.getInt("Max_Attempt");
-        assignment.setMaxAttempt(maxAttempts);
-
-        assignments.add(assignment);
-      }
-
-      rs.close();
-      stmt.close();
-      c.close();
-    } catch (Exception e) {
-      // TODO Error Handling
-      System.err.println(e.getClass().getName() + ": " + e.getMessage());
-    }
-
-    return assignments;
-  }
-
-  /**
-   * Get all assignments of a given course. generateQuestions is false by default.
-   *
-   * @param courseCode the course code of the assignments
-   * @param generateQuestions whether to populate the questions list or not
-   * @return list of assignments belonging to the given course
-   */
-  public List<Assignment> getAssignments(String courseCode, Boolean generateQuestions) {
-    List<Assignment> assignments = new ArrayList<>();
-
-    Connection c;
-    PreparedStatement stmt;
-
-    String sql = "SELECT Id, Assignment_Name, Visibility, Max_Attempt, Open_Time, Close_Time "
-        + "FROM Assignments "
-        + "WHERE Course_Code = ? "
-        + "ORDER BY Id ASC";
-
-    try {
-      c = new PostgreSqlJdbc().getConnection();
-      stmt = c.prepareStatement(sql);
-      stmt.setString(1, courseCode);
-      ResultSet rs = stmt.executeQuery();
-
-      while (rs.next()) {
-        int id = rs.getInt("Id");
-        String name = rs.getString("Assignment_Name");
-        Assignment assignment = new Assignment(name, id);
-        assignment.setVisibility(rs.getBoolean("Visibility"));
-
-        // Returns 0 if value is null, therefore 0 means infinite attempts
-        int maxAttempts = rs.getInt("Max_Attempt");
-        assignment.setMaxAttempt(maxAttempts);
-
-        Date openTime = rs.getTimestamp("Open_Time");
-        Date closeTime = rs.getTimestamp("Close_Time");
-        assignment.setOpenDate(openTime);
-        assignment.setCloseDate(closeTime);
-
-        if (generateQuestions) {
-          assignment.setQuestionList(new QuestionDao().getQuestions(id));
-        }
-        assignments.add(assignment);
-      }
-
-      rs.close();
-      stmt.close();
-      c.close();
-    } catch (Exception e) {
-      // TODO Error Handling
-      System.err.println(e.getClass().getName() + ": " + e.getMessage());
-    }
-
-    return assignments;
-  }
-
-  /**
-   * Gets the ID, name, and Visibility status of all assignments.
-   * @return list of all assignments
-   */
-  public List<Assignment> getAllAssignments() {
-    List<Assignment> assignments = new ArrayList<>();
-
-    Connection c;
-    PreparedStatement stmt;
-
-    String sql = "SELECT Id, Assignment_Name,Visibility, Max_Attempt, Open_Time, Close_Time "
-            + "FROM Assignments";
-
-    try {
-      c = new PostgreSqlJdbc().getConnection();
-      stmt = c.prepareStatement(sql);
-      ResultSet rs = stmt.executeQuery();
-
-      while (rs.next()) {
-        int id = rs.getInt("Id");
-        String name = rs.getString("Assignment_Name");
-        Assignment assignment = new Assignment(name, id);
-        assignment.setVisibility(rs.getBoolean("Visibility"));
-
-        int maxAttempt = rs.getInt("Max_Attempt");
-        Date openTime = rs.getTimestamp("Open_Time");
-        Date closeTime = rs.getTimestamp("Close_Time");
-
-        assignment.setMaxAttempt(maxAttempt);
-        assignment.setOpenDate(openTime);
-        assignment.setCloseDate(closeTime);
-
-        assignments.add(assignment);
-      }
-
-      rs.close();
-      stmt.close();
-      c.close();
-    } catch (Exception e) {
-      // TODO Error Handling
-      System.err.println(e.getClass().getName() + ": " + e.getMessage());
-    }
-
-    return assignments;
-  }
-
-  /**
-   * Add an assignment to the database.
-   *
-   * @param assignmentName name of the assignment
-   * @param courseCode the course code that the assignment belongs to
-   * @return the generated id of the assignment
-   */
-  public int addAssignment(String assignmentName, String courseCode, int maxAttempt) {
-    String message = "-1";
-
-    Connection c;
-    PreparedStatement stmt;
-
-    String sql = "SELECT AddEdit_Assignment(?, ?, ?)";
-
-    try {
-      c = new PostgreSqlJdbc().getConnection();
-      stmt = c.prepareStatement(sql);
-
-      stmt.setInt(1, -1);
-      stmt.setString(2, courseCode);
-      stmt.setString(3, assignmentName);
-
-      ResultSet rs = stmt.executeQuery();
-
-      rs.next();
-
-      message = rs.getString(1);
-
-      rs.close();
-      stmt.close();
-      c.close();
-
-      editAssignmentMaxAttempt(Integer.parseInt(message), maxAttempt);
-
-    } catch (Exception e) {
-      // TODO Error Handling
-      System.err.println(e.getClass().getName() + ": " + e.getMessage());
-    }
-
-    return Integer.parseInt(message);
-  }
-
-  /**
-   * Add an assignment to the database.
-   *
-   * @param assignmentName name of the assignment
-   * @param courseCode the course code that the assignment belongs to
-   * @return the generated id of the assignment
-   */
-  public int addAssignment(String assignmentName, String courseCode) {
-    return addAssignment(assignmentName, courseCode, 0);
-  }
-
-  /**
-   * Edit an Assignment's max number of attempts. Max attempt of 0 means infinite attempts
-   *
-   * @param id the id of an assignment
-   * @param maxAttempt the new max number of attempts
-   * @return whether the edit was successful or not
-   */
-  public Boolean editAssignmentMaxAttempt(int id, int maxAttempt) {
-    Assignment assignment = new Assignment("", id);
-    assignment.setMaxAttempt(maxAttempt);
-    return editAssignmentMaxAttempt(assignment);
-  }
-
-  /**
-   * Edit an Assignment's max number of attempts. Max attempt of 0 means infinite attempts
-   *
-   * @param assignment the assignment object to change with the new max attempt already set
-   * @return whether the edit was successful or not
-   */
-  public Boolean editAssignmentMaxAttempt(Assignment assignment) {
-    Boolean success = false;
-
-    Connection c;
-    PreparedStatement stmt;
-
-    String sql = "UPDATE Assignments SET Max_Attempt = ? WHERE Id = ?";
-
-    try {
-      c = new PostgreSqlJdbc().getConnection();
-      stmt = c.prepareStatement(sql);
-
-      stmt.setInt(1, assignment.getMaxAttempt());
-      stmt.setInt(2, assignment.getId());
-
-      success = stmt.executeUpdate() > 0;
-
-      stmt.close();
-      c.close();
-
-    } catch (Exception e) {
-      // TODO Error Handling
-      System.err.println(e.getClass().getName() + ": " + e.getMessage());
-    }
-
-    return success;
-  }
-
-  /**
-   * Edit the name of a given assignment.
-   *
-   *
-   * @param aId
-   * @param assignName
-   * @return message generated by the database
-   */
-  public String editAssignment(int aId, String assignName) {
-    String message = "";
-
-    Connection c;
-    PreparedStatement stmt;
-
-    String sql = "SELECT AddEdit_Assignment(?, ?, ?)";
-
-    try {
-      c = new PostgreSqlJdbc().getConnection();
-      stmt = c.prepareStatement(sql);
-
-      stmt.setInt(1, aId);
-      stmt.setString(2, null);
-      stmt.setString(3, assignName);
-
-      ResultSet rs = stmt.executeQuery();
-
-      rs.next();
-
-      message = rs.getString(1);
-
-      rs.close();
-      stmt.close();
-      c.close();
-
-    } catch (Exception e) {
-      // TODO Error Handling
-      System.err.println(e.getClass().getName() + ": " + e.getMessage());
-    }
-
-    return message;
-  }
-
-  public boolean changeAssignmentVisibility(int aId, boolean visible) {
-    boolean flag = false;
-    Connection c;
-    PreparedStatement stmt;
-
-    String sql = "UPDATE Assignments SET Visibility=? WHERE Id = ? ";
-
-    try {
-      c = new PostgreSqlJdbc().getConnection();
-      stmt = c.prepareStatement(sql);
-      stmt.setBoolean(1, visible);
-      stmt.setInt(2, aId);
-
-      stmt.execute();
-      stmt.close();
-      c.close();
-      flag = true;
-    } catch (Exception e) {
-      return flag;
-    }
-    return flag;
-
-  }
+	public List<Assignment> getAssignments(String courseCode) {
+		return getAssignments(courseCode, false);
+	}
+
+	/**
+	 * Gets an assignment which has the assignment ID aId.
+	 * 
+	 * @param aId
+	 *            the desired assignment ID.
+	 * @return the Assignment with the id aId.
+	 */
+	public List<Assignment> getAssignments(Integer aId) {
+		List<Assignment> assignments = new ArrayList<>();
+
+		Connection c;
+		PreparedStatement stmt;
+
+		String sql = "SELECT Id, Assignment_Name, Visibility, Max_Attempt,Deadline,Extra_points " + "FROM Assignments "
+				+ "WHERE id = ? ";
+
+		try {
+			c = new PostgreSqlJdbc().getConnection();
+			stmt = c.prepareStatement(sql);
+			stmt.setInt(1, aId);
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				int id = rs.getInt("Id");
+				String name = rs.getString("Assignment_Name");
+				Assignment assignment = new Assignment(name, id);
+				// assignment.setVisibility(rs.getString("Visibility"));
+
+				int maxAttempts = rs.getInt("Max_Attempt");
+				assignment.setMaxAttempt(maxAttempts);
+				Timestamp timestamp = rs.getTimestamp("deadline");
+				if(timestamp != null) {
+					
+					assignment.setEarlySubmissionDeadline(new Date(timestamp.getTime()));
+				}
+				Integer extraPoints = rs.getInt("Extra_points");
+				if(extraPoints!=null) {
+					assignment.setExtraPoints(extraPoints);
+				}
+				
+
+				assignments.add(assignment);
+			}
+
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			// TODO Error Handling
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		return assignments;
+	}
+
+	/**
+	 * Get all assignments of a given course. generateQuestions is false by
+	 * default.
+	 *
+	 * @param courseCode
+	 *            the course code of the assignments
+	 * @param generateQuestions
+	 *            whether to populate the questions list or not
+	 * @return list of assignments belonging to the given course
+	 */
+	public List<Assignment> getAssignments(String courseCode, Boolean generateQuestions) {
+		List<Assignment> assignments = new ArrayList<>();
+
+		Connection c;
+		PreparedStatement stmt;
+
+		String sql = "SELECT Id, Assignment_Name, Visibility, Max_Attempt, Open_Time, Close_Time,Deadline,Extra_points  "
+				+ "FROM Assignments " + "WHERE Course_Code = ? " + "ORDER BY Id ASC";
+
+		try {
+			c = new PostgreSqlJdbc().getConnection();
+			stmt = c.prepareStatement(sql);
+			stmt.setString(1, courseCode);
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				int id = rs.getInt("Id");
+				String name = rs.getString("Assignment_Name");
+				Assignment assignment = new Assignment(name, id);
+				assignment.setVisibility(rs.getBoolean("Visibility"));
+
+				// Returns 0 if value is null, therefore 0 means infinite
+				// attempts
+				int maxAttempts = rs.getInt("Max_Attempt");
+				assignment.setMaxAttempt(maxAttempts);
+
+				Date openTime = rs.getTimestamp("Open_Time");
+				Date closeTime = rs.getTimestamp("Close_Time");
+				assignment.setOpenDate(openTime);
+				assignment.setCloseDate(closeTime);
+				Timestamp timestamp = rs.getTimestamp("deadline");
+				if(timestamp != null) {
+					
+					assignment.setEarlySubmissionDeadline(new Date(timestamp.getTime()));
+				}
+				Integer extraPoints = rs.getInt("Extra_points");
+				if(extraPoints!=null) {
+					assignment.setExtraPoints(extraPoints);
+				}
+				if (generateQuestions) {
+					assignment.setQuestionList(new QuestionDao().getQuestions(id));
+				}
+				assignments.add(assignment);
+			}
+
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			// TODO Error Handling
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		return assignments;
+	}
+
+	/**
+	 * Gets the ID, name, and Visibility status of all assignments.
+	 * 
+	 * @return list of all assignments
+	 */
+	public List<Assignment> getAllAssignments() {
+		List<Assignment> assignments = new ArrayList<>();
+
+		Connection c;
+		PreparedStatement stmt;
+
+		String sql = "SELECT Id, Assignment_Name,Visibility, Max_Attempt, Open_Time, Close_Time,Deadline,Extra_points  "
+				+ "FROM Assignments";
+
+		try {
+			c = new PostgreSqlJdbc().getConnection();
+			stmt = c.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				int id = rs.getInt("Id");
+				String name = rs.getString("Assignment_Name");
+				Assignment assignment = new Assignment(name, id);
+				assignment.setVisibility(rs.getBoolean("Visibility"));
+
+				int maxAttempt = rs.getInt("Max_Attempt");
+				Date openTime = rs.getTimestamp("Open_Time");
+				Date closeTime = rs.getTimestamp("Close_Time");
+
+				assignment.setMaxAttempt(maxAttempt);
+				assignment.setOpenDate(openTime);
+				assignment.setCloseDate(closeTime);
+				Timestamp timestamp = rs.getTimestamp("deadline");
+				if(timestamp != null) {
+					
+					assignment.setEarlySubmissionDeadline(new Date(timestamp.getTime()));
+				}
+				Integer extraPoints = rs.getInt("Extra_points");
+				if(extraPoints!=null) {
+					assignment.setExtraPoints(extraPoints);
+				}
+				assignments.add(assignment);
+			}
+
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			// TODO Error Handling
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		return assignments;
+	}
+
+	/**
+	 * Add an assignment to the database.
+	 *
+	 * @param assignmentName
+	 *            name of the assignment
+	 * @param courseCode
+	 *            the course code that the assignment belongs to
+	 * @return the generated id of the assignment
+	 */
+	public int addAssignment(String assignmentName, String courseCode, int maxAttempt) {
+		String message = "-1";
+
+		Connection c;
+		PreparedStatement stmt;
+
+		String sql = "SELECT AddEdit_Assignment(?, ?, ?)";
+
+		try {
+			c = new PostgreSqlJdbc().getConnection();
+			stmt = c.prepareStatement(sql);
+
+			stmt.setInt(1, -1);
+			stmt.setString(2, courseCode);
+			stmt.setString(3, assignmentName);
+
+			ResultSet rs = stmt.executeQuery();
+
+			rs.next();
+
+			message = rs.getString(1);
+
+			rs.close();
+			stmt.close();
+			c.close();
+
+			editAssignmentMaxAttempt(Integer.parseInt(message), maxAttempt);
+
+		} catch (Exception e) {
+			// TODO Error Handling
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		return Integer.parseInt(message);
+	}
+
+	/**
+	 * Add an assignment to the database.
+	 *
+	 * @param assignmentName
+	 *            name of the assignment
+	 * @param courseCode
+	 *            the course code that the assignment belongs to
+	 * @return the generated id of the assignment
+	 */
+	public int addAssignment(String assignmentName, String courseCode) {
+		return addAssignment(assignmentName, courseCode, 0);
+	}
 
 	public boolean editDeadlineAndExtraPoint(int assignmentId, Date deadline, Integer extraPoints) {
 		boolean success = false;
@@ -371,6 +290,133 @@ public class AssignmentDao {
 		return success;
 	}
 
+	/**
+	 * Edit an Assignment's max number of attempts. Max attempt of 0 means
+	 * infinite attempts
+	 *
+	 * @param id
+	 *            the id of an assignment
+	 * @param maxAttempt
+	 *            the new max number of attempts
+	 * @return whether the edit was successful or not
+	 */
+	public Boolean editAssignmentMaxAttempt(int id, int maxAttempt) {
+		Assignment assignment = new Assignment("", id);
+		assignment.setMaxAttempt(maxAttempt);
+		return editAssignmentMaxAttempt(assignment);
+	}
+
+	/**
+	 * Edit an Assignment's max number of attempts. Max attempt of 0 means
+	 * infinite attempts
+	 *
+	 * @param assignment
+	 *            the assignment object to change with the new max attempt
+	 *            already set
+	 * @return whether the edit was successful or not
+	 */
+	public Boolean editAssignmentMaxAttempt(Assignment assignment) {
+		Boolean success = false;
+
+		Connection c;
+		PreparedStatement stmt;
+
+		String sql = "UPDATE Assignments SET Max_Attempt = ? WHERE Id = ?";
+
+		try {
+			c = new PostgreSqlJdbc().getConnection();
+			stmt = c.prepareStatement(sql);
+
+			stmt.setInt(1, assignment.getMaxAttempt());
+			stmt.setInt(2, assignment.getId());
+
+			success = stmt.executeUpdate() > 0;
+
+			stmt.close();
+			c.close();
+
+		} catch (Exception e) {
+			// TODO Error Handling
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		return success;
+	}
+
+	/**
+	 * Edit the name of a given assignment.
+	 *
+	 * @param assignment
+	 *            an existing assignment but with an edited name
+	 * @return message generated by the database
+	 */
+	public String editAssignment(Assignment assignment) {
+		String message = "";
+
+		Connection c;
+		PreparedStatement stmt;
+
+		String sql = "SELECT AddEdit_Assignment(?, ?, ?)";
+
+		try {
+			c = new PostgreSqlJdbc().getConnection();
+			stmt = c.prepareStatement(sql);
+
+			stmt.setInt(1, assignment.getId());
+			stmt.setString(2, null);
+			stmt.setString(3, assignment.getName());
+
+			ResultSet rs = stmt.executeQuery();
+
+			rs.next();
+
+			message = rs.getString(1);
+
+			rs.close();
+			stmt.close();
+			c.close();
+
+		} catch (Exception e) {
+			// TODO Error Handling
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		return message;
+	}
+
+	public boolean changeAssignmentVisibility(int aId, boolean visible) {
+		boolean flag = false;
+		Connection c;
+		PreparedStatement stmt;
+
+		String sql = "UPDATE Assignments SET Visibility=? WHERE Id = ? ";
+
+		try {
+			c = new PostgreSqlJdbc().getConnection();
+			stmt = c.prepareStatement(sql);
+			stmt.setBoolean(1, visible);
+			stmt.setInt(2, aId);
+
+			stmt.execute();
+			stmt.close();
+			c.close();
+			flag = true;
+		} catch (Exception e) {
+			return flag;
+		}
+		return flag;
+
+	}
+
+	/**
+	 * Get a student's used number of attempts for an assignment.
+	 *
+	 * @param assignmentId
+	 *            the id of the assignment
+	 * @param studentUtorId
+	 *            the utor id of the student
+	 * @return the used number of attempts
+	 */
 	public int getStudentUsedAttempts(int assignmentId, String studentUtorId) {
 		int usedAttempts = 0;
 
@@ -464,6 +510,7 @@ public class AssignmentDao {
 
 		Connection c;
 		PreparedStatement stmt;
+
 		String sql = "SELECT Course_Code FROM Assignments WHERE Id=?";
 
 		try {
