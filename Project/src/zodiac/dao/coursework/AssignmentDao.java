dd9e8150ebb53b7edcbae0ce223a140ada5ea74d
Junzhi Chen <junzhichen@Junzhis-MacBook.local>
2017-11-27 01:44:42 -0500

    modify assignmentDao

--- Project/src/zodiac/dao/coursework/AssignmentDao.java
+++ Project/src/zodiac/dao/coursework/AssignmentDao.java
@@ -31,7 +31,7 @@ public class AssignmentDao {
     Connection c;
     PreparedStatement stmt;
 
-    String sql = "SELECT Id, Assignment_Name, Visibility, Max_Attempt,Deadline,Extra_points "
+    String sql = "SELECT Id, Assignment_Name, Visibility, Max_Attempt, Open_Time, Close_Time,Deadline,Extra_points "
             + "FROM Assignments "
             + "WHERE id = ? ";
 
